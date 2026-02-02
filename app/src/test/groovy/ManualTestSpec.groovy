e5b1b8c47786f6e858c6f29e05d8f6dd648b40a8
ma.moravec <marek.moravec@quadient.com>
2026-02-02 17:59:45 +0100

    dewélete

--- app/src/test/groovy/ManualTestSpec.groovy
+++
@@ -1,1 +0,0 @@
-I apologize for the repeated response. To assist you properly, I need the specific requirements or description details. Could you please provide the content or details from the `$node["HTTP Request"].json.description`?
