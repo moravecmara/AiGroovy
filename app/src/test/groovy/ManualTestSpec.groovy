62c041b78097255b0434bee8230f3250d3806ce1
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-02 17:59:57 +0100

    Commit

---
+++ app/src/test/groovy/ManualTestSpec.groovy
@@ -0,0 +1,13 @@
+package my.package
+
+import org.junit.Test
+import static org.junit.Assert.*
+
+class SampleTest {
+
+    @Test
+    void testSomething() {
+        // Your test logic here
+        assertTrue(true)
+    }
+}
