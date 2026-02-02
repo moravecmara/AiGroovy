1c89b543237bd8cb2ca1c681c89be7b189ccf7e6
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-02 15:43:52 +0100

    Commit

---
+++ app/src/test/groovy/ManualTestSpec.groovy
@@ -0,0 +1,11 @@
+package com.example.test
+
+import groovy.util.GroovyTestCase
+
+class SampleTest extends GroovyTestCase {
+
+    void testExample() {
+        // Your test logic here
+        assertEquals 2, 1 + 1
+    }
+}
