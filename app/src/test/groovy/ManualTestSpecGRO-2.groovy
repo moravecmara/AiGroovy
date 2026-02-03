2612d7240e4f6edfbba5b83c5163245a7388963e
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-03 10:10:57 +0100

    Commit

---
+++ app/src/test/groovy/ManualTestSpecGRO-2.groovy
@@ -0,0 +1,12 @@
+package com.example.testing
+
+import org.junit.jupiter.api.Test
+import static org.junit.jupiter.api.Assertions.assertTrue
+
+class SampleTest {
+
+    @Test
+    void testSimpleAssertion() {
+        assertTrue(true)
+    }
+}
