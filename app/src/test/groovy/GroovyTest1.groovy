14f74e7377bd56b6a246c969fc1c0432db264d35
ma.moravec <marek.moravec@quadient.com>
2026-02-04 10:23:33 +0100

    dewélete

--- app/src/test/groovy/GroovyTest1.groovy
+++ app/src/test/groovy/GroovyTest1.groovy
@@ -1,13 +1,13 @@
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class GroovyTest1 {
     private static int a = 2
-    private static int boke = 5
+    private static int b = 5
 
     @Test
     void mathsOp() {
-        assert add(a, boke) == 
-        assert multiply(a, boke) == 19
+        assert add(a, b) == 5
+        assert multiply(a, b) == 19
     }
 
     int add(int a, int b) {
@@ -18,3 +18,4 @@ class GroovyTest1 {
         return a * b
     }
 }
+
