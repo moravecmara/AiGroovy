268fb126772b807a45252e4ed1b3a4a4cd3fd7fd
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-05 16:45:43 +0100

    feat: Create initial Groovy test file for GRO-1

---
+++ app/src/test/groovy/GroovyTestGRO-1.groovy
@@ -0,0 +1,12 @@
+package com.example
+
+import org.junit.Test
+
+class GroovyTestGRO-1 {
+
+    @Test
+    void testInitialSetup() {
+        // Initial test logic goes here
+        assert true
+    }
+}
