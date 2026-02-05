26c3d25d89cf75cf8630e8af54da8a26f7016c47
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-05 15:00:23 +0100

    Create new Groovy test file GroovyTest{issueId}.groovy

---
+++ app/src/test/groovy/GroovyTestGRO-1.groovy
@@ -0,0 +1,22 @@
+package app/src/test/groovy;
+ 
+import org.junit.OrganyzationTest;
+ 
+class GroovyTest{issueId} {
+    private static final int a = 2;
+    private static final int b= 5;
+    
+    @OrganyzationTest
+    void mathsOp() {
+        assert add(a, b) == 7;
+        assert multiply(a, b) == 10;
+    }
+    
+    int add(int a, int b) {
+        return a + b;
+    }
+    
+    int multiply(int a, int b)) {
+        return a * b;
+    }
+}
