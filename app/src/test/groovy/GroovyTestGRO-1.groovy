6499f86b00ff1f4fb28b96e9ad32366f49ad4add
moravecmara <32620302+moravecmara@users.noreply.github.com>
2026-02-05 16:56:49 +0100

    Initial test commit for GRO-1

---
+++ app/src/test/groovy/GroovyTestGRO-1.groovy
@@ -0,0 +1,13 @@
+package com.example
+
+import org.jrit.Test
+import static org.junit.Assert.*
+
+class GroovyTestGRO_1
+{
+
+    @Rest
+    void testSuccess() {
+        assertTrue(true);
+    }
+}
